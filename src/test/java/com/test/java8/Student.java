c4ef0f2bb96a32a525c068f6bb6addc6943cac28
胡哲 <huzhe@xiaoshouyi.com>
2020-02-12 13:59:36 +0800

    增加测试demo

--- src/test/java/com/test/java8/Student.java
+++ src/test/java/com/test/java8/Student.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class Student {
     private String name;
     private Integer age;
+    private String property;
 }
--- src/test/java/com/test/java8/TestCase.java
+++ src/test/java/com/test/java8/TestCase.java
@@ -1,5 +1,7 @@
 package com.test.java8;
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,5 +24,45 @@ public class TestCase {
         executorService.shutdown();
         Student student = submit.get();
         System.out.println(student);//
+    }
+
+    @Test
+    public void demo2() throws ExecutionException, InterruptedException {
+        String s = "{\"hasEditBusinessTypeToolbarButton\":true}";
+        JSONObject jsonObject = JSONObject.parseObject(s);
+        jsonObject.put("name","xiaoming");
+
+        System.out.println("--------------------------" + jsonObject.toJSONString());//
+    }
+
+    @Test
+    public void demo3() throws ExecutionException, InterruptedException {
+        /*JSONObject jsonObject = new JSONObject();
+        jsonObject.put("hasEditBusinessTypeToolbarButton", true);
+        property = jsonObject.toJSONString();
+        layout.setProperty(property);*/
+        Student student = new Student();
+        student.setName("jack");
+        student.setAge(12);
+        student.setProperty("{\"boolean\":false}");
+        String property = student.getProperty();
+        JSONObject jsonObject = new JSONObject();
+        if(StringUtils.isNotBlank(property)){
+            jsonObject = JSONObject.parseObject(property);
+        }
+        jsonObject.put("boolean",true);
+        property = jsonObject.toJSONString();
+        student.setProperty(property);
+        System.out.println(student);//
+    }
+    @Test
+    public void demo4() throws ExecutionException, InterruptedException {
+        String property = "{\"name\":\"jack\"}";
+        Boolean aBoolean = JSONObject.parseObject(property).getBoolean("hasEditBusinessTypeToolbarButton");
+        System.out.println(aBoolean);
+        System.out.println(aBoolean == true);
+        /*if (property != null && JSONObject.parseObject(property).getBoolean("hasEditBusinessTypeToolbarButton") == true) {
+            return;
+        }*/
     }
 }
